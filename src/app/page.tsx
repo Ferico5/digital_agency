a3b18ab38e0ea1c96694728c352fe641a1cdbb82
ferico <huangferico@gmail.com>
2025-10-13 21:49:31 +0700

    finish home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+
 const HomePage = () => {
   return (
-    <div className="">Home Page</div>
-  )
-}
+    <div className="flex justify-between items-center min-h-[90vh]">
+      {/* left side content */}
+      <div className="flex flex-col justify-center w-[40%]">
+        <h1 className='text-5xl font-bold leading-[1.3] mb-7 bg-gradient-to-b from-[#194c33] to-[#bbb] 
+          bg-clip-text text-transparent'>Better design for your digital products</h1>
+        <p className='textw-[50%]'>Turning your idea into Reality. We bring together the teams rom the global team industry</p>
+      </div>
+      <Image src="/hero.png" alt="Hero Image" width={400} height={400} />
+      {/* right side content */}
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
