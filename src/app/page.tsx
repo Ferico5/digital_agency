43d8d0d991ef673511f0b1a9863b56f4922c017e
ferico <huangferico@gmail.com>
2025-10-14 13:31:43 +0700

    add button link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,27 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const HomePage = () => {
   return (
     <div className="flex justify-between items-center min-h-[88vh]">
       {/* left side content */}
-      <div className="flex flex-col justify-center w-[40%]">
-        <h1 className='text-5xl font-bold leading-[1.3] mb-7 bg-gradient-to-b from-[#194c33] to-[#bbb] 
-          bg-clip-text text-transparent'>Better design for your digital products</h1>
-        <p className='textw-[50%]'>Turning your idea into Reality. We bring together the teams rom the global team industry</p>
+      <div className="flex flex-col justify-between w-[50%]">
+        <h1
+          className="text-6xl font-bold leading-[1.3] mb-7 bg-gradient-to-b from-[#194c33] to-[#bbb] 
+          bg-clip-text text-transparent"
+        >
+          Better design for your digital products.
+        </h1>
+        <p className="textw-[50%]">Turning your idea into Reality. We bring together the teams rom the global team industry</p>
+        <div className="mt-7">
+          <Link href="/portfolio" className="bg-[#53C38B] p-3 rounded-sm">
+            See Our Works
+          </Link>
+        </div>
       </div>
       {/* right side content */}
-      <div className='animate-float'>
-      <Image src="/hero.png" alt="Hero Image" width={400} height={400} />
-
+      <div className="animate-float">
+        <Image src="/hero.png" alt="Hero Image" width={400} height={400} />
       </div>
     </div>
   );
