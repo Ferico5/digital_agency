686284c88d4958decab8f4da64c0dc4fec3b89e9
ferico <huangferico@gmail.com>
2025-10-15 15:38:32 +0700

    dummy data desing for blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const BlogPage = () => {
   return (
-    <div>Blog page</div>
-  )
-}
+    <div className="flex flex-col">
+      <Link href="/" className="mb-10">
+        <div className="flex">
+          <Image src="/apps.jpg" alt="Blog Image" width={400} height={370}></Image>
+          <div className="flex flex-col justify-center ml-10">
+            <h1 className="font-bold text-2xl mb-5">Title Blog</h1>
+            <p className="text-[#999]">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime commodi fuga eaque natus, beatae illum fugiat eum quos? Ratione enim deleniti debitis alias, culpa impedit delectus vitae sed exercitationem. Perspiciatis magni
+              cum error deleniti amet, reiciendis nobis unde rem cumque rerum ex aut incidunt, harum, tenetur perferendis. Ratione, explicabo harum?
+            </p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default BlogPage
+export default BlogPage;
