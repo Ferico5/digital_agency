6f2bdc9ad85d5f519b3b571a9b296a77ffe19c58
ferico <huangferico@gmail.com>
2025-10-16 15:53:43 +0700

    add content to blog model

--- src/prisma.ts
+++ src/prisma.ts
@@ -6,9 +6,30 @@ const seedBlog = async () => {
   if (count === 0) {
     await prisma.blog.createMany({
       data: [
-        { title: 'Blog 1', description: "Description Blog 1", image: '/apps.jpg', url: "/" },
-        { title: 'Blog 2', description: "Description Blog 2", image: '/apps.jpg', url: "/" },
-        { title: 'Blog 3', description: "Description Blog 3", image: '/apps.jpg', url: "/" },
+        { 
+            title: 'Blog 1', 
+            description: "Description Blog 1", 
+            content: 
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
+            image: '/apps.jpg', 
+            url: "/" 
+        },
+        { 
+            title: 'Blog 2', 
+            description: "Description Blog 2", 
+            content: 
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
+            image: '/apps.jpg', 
+            url: "/" 
+        },
+        { 
+            title: 'Blog 3', 
+            description: "Description Blog 3", 
+            content: 
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
+            image: '/apps.jpg', 
+            url: "/" 
+        },
       ],
     });
   }
