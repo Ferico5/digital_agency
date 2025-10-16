41c1dda08f6d6538e862ea8f7e17524e1572d791
ferico <huangferico@gmail.com>
2025-10-16 09:44:23 +0700

    change file name components

---
+++ src/components/BlogContent/BlogContent.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface BlogContentProps {
+  url: string;
+  image: string;
+  title: string;
+  description: string;
+}
+
+const BlogContent = ({ url, image, title, description }: BlogContentProps) => {
+  return (
+    <Link href={url} className="mb-10">
+      <div className="flex">
+        <Image src={image} alt="Blog Image" width={400} height={370}></Image>
+        <div className="flex flex-col justify-center ml-10">
+          <h1 className="font-bold text-2xl mb-5">{title}</h1>
+          <p className="text-[#999]">{description}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default BlogContent;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  {
+    id: 1,
+    title: 'Home',
+    url: '/',
+  },
+  {
+    id: 2,
+    title: 'Portfolio',
+    url: '/portfolio',
+  },
+  {
+    id: 3,
+    title: 'Blog',
+    url: '/blog',
+  },
+  {
+    id: 4,
+    title: 'About',
+    url: '/about',
+  },
+  {
+    id: 5,
+    title: 'Contact',
+    url: '/contact',
+  },
+  {
+    id: 6,
+    title: 'Dashboard',
+    url: '/dashboard',
+  },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex justify-between mb-8">
+      <Link href="/">
+        <h1>LamaDev</h1>
+      </Link>
+      <div className="flex gap-5">
+        {links.map((link) => (
+          <Link key={link.id} href={link.url} className={`hover:text-green-400 transition ${pathname === link.url ? 'text-green-500 font-semibold' : 'text-white'}`}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
