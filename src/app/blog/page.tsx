db4f1ea6d9e3ca6c9701431e969ff8f9b46a8d95
ferico <huangferico@gmail.com>
2025-10-16 11:07:05 +0700

    add GET all Blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,21 +1,21 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import BlogContent from '@/components/BlogContent/BlogContent';
+import { getAllBlogs } from '@/prisma';
 
-const BlogPage = () => {
+export type Blog = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+};
+
+const BlogPage = async () => {
+  const blogs = await getAllBlogs();
   return (
     <div className="flex flex-col">
-      <Link href="/" className="mb-10">
-        <div className="flex">
-          <Image src="/apps.jpg" alt="Blog Image" width={400} height={370}></Image>
-          <div className="flex flex-col justify-center ml-10">
-            <h1 className="font-bold text-2xl mb-5">Title Blog</h1>
-            <p className="text-[#999]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime commodi fuga eaque natus, beatae illum fugiat eum quos? Ratione enim deleniti debitis alias, culpa impedit delectus vitae sed exercitationem. Perspiciatis magni
-              cum error deleniti amet, reiciendis nobis unde rem cumque rerum ex aut incidunt, harum, tenetur perferendis. Ratione, explicabo harum?
-            </p>
-          </div>
-        </div>
-      </Link>
+      {blogs.map((blog) => (
+        <BlogContent key={blog.id} url={blog.url} image={blog.image} title={blog.title} description={blog.description}></BlogContent>
+      ))}
     </div>
   );
 };
