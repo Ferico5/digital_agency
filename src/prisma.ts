d55dfa6d7cdb3b8ec2301a86f838ebfc4363af39
ferico <huangferico@gmail.com>
2025-10-17 14:58:48 +0700

    add getBlogById and create url from UUID automatically

--- src/prisma.ts
+++ src/prisma.ts
@@ -4,34 +4,38 @@ const prisma = new PrismaClient();
 const seedBlog = async () => {
   const count = await prisma.blog.count();
   if (count === 0) {
-    await prisma.blog.createMany({
-      data: [
-        { 
-            title: 'Blog 1', 
-            description: "Description Blog 1", 
-            content: 
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
-            image: '/apps.jpg', 
-            url: "/" 
-        },
-        { 
-            title: 'Blog 2', 
-            description: "Description Blog 2", 
-            content: 
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
-            image: '/apps.jpg', 
-            url: "/" 
-        },
-        { 
-            title: 'Blog 3', 
-            description: "Description Blog 3", 
-            content: 
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam placeat eum itaque veniam rerum voluptatem numquam ab fugiat laboriosam sequi, accusamus sint vel dolor delectus amet dolorem corrupti culpa ea assumenda nisi repudiandae nobis id impedit? Hic accusantium dolorem rerum alias dolores. Ea quaerat dolores blanditiis ducimus libero expedita omnis praesentium ipsum, magnam vitae fuga amet quia totam rerum cum. Veritatis provident, non recusandae, maxime dolorem, sint nihil totam esse laborum excepturi dolores nulla quasi nobis temporibus aspernatur? Nam blanditiis libero adipisci quod! Tempore ullam possimus cupiditate quibusdam, dolor doloribus illo unde doloremque. Veniam rerum quaerat earum ad harum quam.", 
-            image: '/apps.jpg', 
-            url: "/" 
-        },
-      ],
-    });
+    const blogsData = [
+      {
+        title: 'Blog 1',
+        description: 'Description Blog 1',
+        content: 'Lorem ipsum dolor sit amet...',
+        image: '/apps.jpg',
+        author: 'John Doe',
+      },
+      {
+        title: 'Blog 2',
+        description: 'Description Blog 2',
+        content: 'Lorem ipsum dolor sit amet...',
+        image: '/apps.jpg',
+        author: 'John Doe',
+      },
+      {
+        title: 'Blog 3',
+        description: 'Description Blog 3',
+        content: 'Lorem ipsum dolor sit amet...',
+        image: '/apps.jpg',
+        author: 'John Doe',
+      },
+    ];
+
+    // create blogs and generate UUID automatically
+    for (const blog of blogsData) {
+      const newBlog = await prisma.blog.create({ data: blog });
+      await prisma.blog.update({
+        where: { id: newBlog.id },
+        data: { url: `/blog/${newBlog.id}` },
+      });
+    }
   }
 };
 
@@ -39,6 +43,12 @@ seedBlog();
 
 export async function getAllBlogs() {
   return prisma.blog.findMany();
+}
+
+export async function getBlogById(id: string) {
+  return prisma.blog.findUnique({
+    where: { id },
+  });
 }
 
 export default prisma;
